fdcc37169cf3d59c505f68c69af4806d825e55e3
dhekmass12 <dimas.tri01@ui.ac.id>
2021-12-31 17:34:07 +0700

    fix cannot run problem

--- proyek_akhir/lib/page/oksigen.dart
+++ proyek_akhir/lib/page/oksigen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:footer/footer.dart';
-import 'package:footer/footer_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:proyek_akhir/page/forum.dart';
 
@@ -275,7 +273,7 @@ class MyState extends State<MyStatefulWidget> {
             ],
           ),
         ),
-        body: FooterView(
+        body: Column(
             children: <Widget>[
               new Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -418,105 +416,14 @@ class MyState extends State<MyStatefulWidget> {
                   ),
                 ],
               ),
-            ],
-            footer: new Footer(
-              child: new Padding(
-                padding: EdgeInsets.all(5.0),
-                child: new Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      new Center(
-                        child: new Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            new Container(
-                                height: 45.0,
-                                width: 45.0,
-                                child: Center(
-                                  child: Card(
-                                    elevation: 5.0,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                          25.0), // half of height and width of Image
-                                    ),
-                                    child: IconButton(
-                                      icon: new Icon(
-                                        Icons.facebook,
-                                        size: 20.0,
-                                      ),
-                                      color: Color(0xFF162A49),
-                                      onPressed: () {},
-                                    ),
-                                  ),
-                                )),
-                            new Container(
-                                height: 45.0,
-                                width: 45.0,
-                                child: Center(
-                                  child: Card(
-                                    elevation: 5.0,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                          25.0), // half of height and width of Image
-                                    ),
-                                    child: IconButton(
-                                      icon: FaIcon(FontAwesomeIcons.twitter,
-                                          size: 20.0),
-                                      color: Color(0xFF162A49),
-                                      onPressed: () {
-                                        // TODO
-                                      },
-                                    ),
-                                  ),
-                                )),
-                            new Container(
-                                height: 45.0,
-                                width: 45.0,
-                                child: Center(
-                                  child: Card(
-                                    elevation: 5.0,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                          25.0), // half of height and width of Image
-                                    ),
-                                    child: IconButton(
-                                      icon: FaIcon(
-                                        FontAwesomeIcons.globe,
-                                        size: 20.0,
-                                      ),
-                                      color: Color(0xFF162A49),
-                                      onPressed: () {},
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                      Text(
-                        'Copyright ©2020, All Rights Reserved by PBP D-05',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 10.0,
-                            color: Color(0xFF162A49)),
-                      ),
-                      Text(
-                        'Powered by Flutter',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 10.0,
-                            color: Color(0xFF162A49)),
-                      ),
-                    ]),
-              ),
-            )),
+            ],),
         floatingActionButton: new FloatingActionButton(
             elevation: 10.0,
             child: new Icon(Icons.chat),
             backgroundColor: Color(0xFF162A49),
             onPressed: () {
               // TODO
-            }),
+        }),
       ),
     );
   }
